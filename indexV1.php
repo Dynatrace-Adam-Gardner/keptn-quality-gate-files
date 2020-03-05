37b3f24b0f750e728fb6259472d5a28dc5e2d584
Adam Gardner <adam.gardner@dynatrace.com>
2020-03-05 15:01:34 +1000

    Create indexV1.php

---
+++ indexV1.php
@@ -0,0 +1,23 @@
+<?php
+// PHP sleeps for 0 seconds before loading page.
+sleep(0);
+?>
+<!doctype html>
+<html>
+<head>
+<title>Delay Demo System: v1</title>
+<style>
+#header {
+  background-color: cornflowerblue;
+}
+</style>
+</head>
+<body>
+<h1 id="header">Delay Demo System: v1</h1>
+<p>Delay Demonstration system.
+<ul>
+  <li>v1 has no delay.</li>
+  <li>v2 has a 3 second page load delay.</li>
+  <li>v3 has 1 second page load delay.</p>
+</body>
+</html>
