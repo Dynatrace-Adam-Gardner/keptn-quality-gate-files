3ab9bf6fa84aa8ccc10b56dbc83191b93e0c128c
Adam Gardner <adam.gardner@dynatrace.com>
2020-03-09 11:07:53 +1000

    Create releaseV2.sh

---
+++ releaseV2.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+####################################
+# WARNING: DO NOT MODIFY THIS FILE #
+####################################
+
+tenant=$(cat websiteSetup.sh | grep "tenant=" | cut -s -d "=" -f2)
+api_token=$(cat websiteSetup.sh | grep "api_token=" | cut -s -d "=" -f2)
+currentTimeMillis=$(date +%s%N | cut -b1-13)
+
+# Push V2 Webpage
+sudo wget https://raw.githubusercontent.com/Dynatrace-Adam-Gardner/keptn-quality-gate-files/master/indexV2.php -O /var/www/html/index.php
+
+# Push DT Event
+curl -X POST "https://$tenant/api/v1/events" -H "accept: application/json" -H "Authorization: Api-Token $api_token" -H "Content-Type: application/json" -d "{ \"eventType\": \"CUSTOM_DEPLOYMENT\", \"start\": $currentTimeMillis, \"end\": $currentTimeMillis, \"attachRules\": { \"tagRule\": [{ \"meTypes\": [\"SERVICE\"], \"tags\": [{ \"context\": \"CONTEXTLESS\", \"key\": \"keptn_deployment\" }, { \"context\": \"CONTEXTLESS\", \"key\": \"keptn_project\", \"value\": \"website\" }, { \"context\": \"CONTEXTLESS\", \"key\": \"keptn_service\", \"value\": \"front-end\" }, { \"context\": \"CONTEXTLESS\", \"key\": \"keptn_stage\", \"value\": \"quality\" } ] }] }, \"source\": \"Pipeline Tool\", \"deploymentName\": \"Release v2\", \"deploymentVersion\": \"v2\"}"
